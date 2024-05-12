0c3772d855f691654cb3e58c9918fedbf4863a76
Sharmin2622 <islam.sharmin111@gmail.com>
2024-05-12 20:47:28 +0600

    faq section added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import Banner from "../shared/Banner";
 import Faq from "../shared/Faq";
+import Features from "../shared/Features";
 
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
+            <Features></Features>
             <Faq></Faq>
         </div>
     );
--- src/shared/Features.jsx
+++ src/shared/Features.jsx
@@ -1,11 +1,9 @@
 import { useEffect, useState } from "react";
-import { FaChevronDown } from "react-icons/fa";
 
 
-const Assignments = () => {
+const Features = () => {
 
     const [assignments, setAssignments] = useState([]);
-    // const [selectedLevel, setSelectedLevel] = useState('');
 
     useEffect(() => {
         fetch('http://localhost:5000/assignments')
@@ -14,18 +12,13 @@ const Assignments = () => {
     }, [])
 
     return (
-        <div className="mt-6">
-            <details className="dropdown mb-6">
-                <summary className="m-1 btn bg-yellow-500 text-black">Level <FaChevronDown /></summary>
-                <ul className="p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-52">
-                    <li><a>Easy</a></li>
-                    <li><a>Medium</a></li>
-                    <li><a>Hard</a></li>
-                </ul>
-            </details>
+        <div className="mt-14">
+            <div className='text-center mb-14'>
+            <h2 className='text-4xl font-bold text-yellow-600 mb-2'>Our Features</h2>
+            </div>
             <div className="grid gap-8 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
                 {
-                    assignments.map(assignment => <div key={assignment._id}>
+                    assignments?.slice(0, 6).map(assignment => <div key={assignment._id}>
                         <div className="card bg-base-100 shadow-xl">
                             <figure className="px-8 pt-8">
                                 <img src={assignment.photo} alt="Shoes" className="rounded-xl h-52 w-full" />
@@ -38,8 +31,6 @@ const Assignments = () => {
                                 </div>
                                 <div className="card-actions gap-4 mt-3">
                                     <button className="btn btn-success text-white">Details</button>
-                                    <button className="btn btn-error text-white">Delete</button>
-                                    <button className="btn btn-warning text-white">Update</button>
                                 </div>
                             </div>
                         </div>
@@ -50,4 +41,4 @@ const Assignments = () => {
     );
 };
 
-export default Assignments;
+export default Features;
